d8e0e8fe8b431e4dae408cf54c9cca8042032f13
d11210920 <109078590+d11210920@users.noreply.github.com>
2023-11-13 13:45:34 +0900

    Revert "Add files via upload"

--- mumomu_project/src/main/java/MUMOMU/mumomu_project/controller/JsonResponse.java
+++
@@ -1,9 +0,0 @@
-package MUMOMU.mumomu_project.controller;
-
-public class JsonResponse {
-    private String status;
-
-    public JsonResponse(String status){
-        this.status=status;
-    }
-}
